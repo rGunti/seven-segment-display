b0a352e309e36dbacabe7cb4d65493a7784439ef
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2023-12-17 21:22:55 +0100

    finished font for matrix display

--- src/demo.ts
+++ src/demo.ts
@@ -8,6 +8,7 @@ import { SEVEN_FONT, SIXTEEN_FONT } from './segment-display/fonts';
 import { DotMatrixDisplay } from './segment-display/matrix';
 import { MATRIX_DEBUG_FONT, MATRIX_FONT } from './segment-display/matrix/font';
 import './style.scss';
+import { repeatArr } from './utils';
 
 function generateCharArray(...range: [number, number][]): string[] {
   const output = [];
@@ -41,70 +42,50 @@ const CHARS_TO_SHOW = generateCharArray(
 */ [32, 127],
 ); /*.concat(['.', ':', '-', '_'])*/
 
+const PARAM = new URLSearchParams(window.location.search);
+const CHAR_COUNT_STR = PARAM.get('count') || '10';
+const CHAR_COUNT = parseInt(CHAR_COUNT_STR, 10);
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const HOST = appRoot.querySelector<HTMLDivElement>('#sixteen-seg-display')!;
-const SIXTEEN_SEGS: SegmentDisplay[] = [
-  new SixteenSegmentDisplay(HOST),
-  new SixteenSegmentDisplay(HOST),
-  new SixteenSegmentDisplay(HOST),
-  new SixteenSegmentDisplay(HOST),
-  new SixteenSegmentDisplay(HOST),
-  new SixteenSegmentDisplay(HOST),
-  new SixteenSegmentDisplay(HOST),
-];
+const SIXTEEN_SEGS: SegmentDisplay[] = repeatArr(
+  () => new SixteenSegmentDisplay(HOST),
+  CHAR_COUNT,
+);
 const CONTROLLER = new SegmentDisplayController(SIXTEEN_SEGS, SIXTEEN_FONT);
-CONTROLLER.show('§§§§§§§');
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const REF_HOST = appRoot.querySelector<HTMLDivElement>('#ref-display')!;
-const SEVEN_SEGS: SegmentDisplay[] = [
-  new FancySevenSegmentDisplay(REF_HOST),
-  new FancySevenSegmentDisplay(REF_HOST),
-  new FancySevenSegmentDisplay(REF_HOST),
-  new FancySevenSegmentDisplay(REF_HOST),
-  new FancySevenSegmentDisplay(REF_HOST),
-  new FancySevenSegmentDisplay(REF_HOST),
-  new FancySevenSegmentDisplay(REF_HOST),
-];
+const SEVEN_SEGS: SegmentDisplay[] = repeatArr(
+  () => new FancySevenSegmentDisplay(REF_HOST),
+  CHAR_COUNT,
+);
 const REF_CONTROLLER = new SegmentDisplayController(SEVEN_SEGS, SEVEN_FONT);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const MATRIX_HOST = appRoot.querySelector<HTMLDivElement>('#matrix-display')!;
-const MATRIX_SEGS: SegmentDisplay[] = [
-  new DotMatrixDisplay(MATRIX_HOST),
-  new DotMatrixDisplay(MATRIX_HOST),
-  new DotMatrixDisplay(MATRIX_HOST),
-  new DotMatrixDisplay(MATRIX_HOST),
-  new DotMatrixDisplay(MATRIX_HOST),
-  new DotMatrixDisplay(MATRIX_HOST),
-  new DotMatrixDisplay(MATRIX_HOST),
-];
+const MATRIX_SEGS: SegmentDisplay[] = repeatArr(
+  () => new DotMatrixDisplay(MATRIX_HOST),
+  CHAR_COUNT,
+);
 const MATRIX_CONTROLLER = new SegmentDisplayController(
   MATRIX_SEGS,
   MATRIX_FONT,
 );
-MATRIX_CONTROLLER.show('4567890');
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const MATRIX_DEBUG_HOST = appRoot.querySelector<HTMLDivElement>(
   '#matrix-debug-display',
 )!;
-const MATRIX_DEBUG_SEGS: SegmentDisplay[] = [
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-  new DotMatrixDisplay(MATRIX_DEBUG_HOST),
-];
+const MATRIX_DEBUG_SEGS: SegmentDisplay[] = repeatArr(
+  () => new DotMatrixDisplay(MATRIX_DEBUG_HOST),
+  10,
+);
 const MATRIX_DEBUG_CONTROLLER = new SegmentDisplayController(
   MATRIX_DEBUG_SEGS,
   MATRIX_DEBUG_FONT,
 );
-MATRIX_DEBUG_CONTROLLER.show('§§§§§§§');
 
-const PARAM = new URLSearchParams(window.location.search);
 const DISPLAY_STR = PARAM.get('displayStr');
 const DISABLE_INTERVAL = DISPLAY_STR || PARAM.get('disableInterval') === '1';
 
@@ -140,6 +121,8 @@ window['DISPLAY'] = {
     REF_CONTROLLER.show(str);
     MATRIX_CONTROLLER.show(str);
     MATRIX_DEBUG_CONTROLLER.show(str);
-    window.location.search = `?displayStr=${encodeURIComponent(str)}`;
+    window.location.search = `?displayStr=${encodeURIComponent(
+      str,
+    )}&count=${CHAR_COUNT}`;
   },
 };
